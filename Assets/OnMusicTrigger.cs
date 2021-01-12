e324cc5a6b42415dc5f58cf4abe46fcd1b810e6d
Eliac11 <58089493+Eliac11@users.noreply.github.com>
2021-01-12 14:51:13 +0300

    сделал забор 3 лвл , накинь там скрипты для перехода , воду хз как ограничить сделал только что не нырнуть

---
+++ Assets/OnMusicTrigger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnMusicTrigger : MonoBehaviour
+{
+    public AudioSource aaa;
+
+    void OnCollisionEnter(Collision over)
+    {
+        Debug.Log("111");
+        if (over.gameObject.CompareTag("Player"))
+        {
+            aaa.Play();
+        }
+    }
+}
