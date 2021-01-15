0082506514b9fd1f4dfc77211615c1bbc1ebea96
RebuffedFaun311 <superduperalex18@outlook.com>
2021-01-15 21:50:41 +0300

    Ограничил озеро,поставил пикап ремингтона,доделал врагов

---
+++ Assets/Scripts/EnemyImmortal.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyImmortal : MonoBehaviour
+{
+    public GameObject player;
+    public float dist;
+    NavMeshAgent nav;
+    public float Radius = 70;
+    void Start()
+    {
+        nav = GetComponent<NavMeshAgent> ();
+    }
+
+    void Update()
+    {
+        dist = Vector3.Distance (player.transform.position, transform.position);
+        if(dist > Radius)
+        { 
+           nav.enabled = false;
+           gameObject.GetComponent<Animator>().SetTrigger("Idle");
+        }
+        if(dist < Radius & dist>2f)
+        {
+            nav.enabled = true;
+            nav.SetDestination(player.transform.position);
+            gameObject.GetComponent<Animator>().SetTrigger("Run");
+        }
+        
+        if(dist < 3f)
+        {
+            gameObject.GetComponent<Animator>().SetTrigger("Hit");
+        }
+    }
+}
---
+++ Assets/Scripts/FastEnemy.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class FastEnemy : MonoBehaviour
+{
+    public GameObject player;
+    public float dist;
+    NavMeshAgent nav;
+    public float Radius = 25;
+    public float HP = 75;
+    public GameObject Ragdoll;
+    void Start()
+    {
+        nav = GetComponent<NavMeshAgent> ();
+    }
+
+    void Update()
+    {
+        if(HP <= 0)
+        {
+            Instantiate(Ragdoll,transform.position, transform.rotation);
+            Destroy (gameObject);
+        }
+
+        dist = Vector3.Distance (player.transform.position, transform.position);
+        if(dist > Radius)
+        { 
+           nav.enabled = false;
+           gameObject.GetComponent<Animator>().SetTrigger("Idle");
+        }
+        if(dist < Radius & dist>2f)
+        {
+            nav.enabled = true;
+            nav.SetDestination(player.transform.position);
+            gameObject.GetComponent<Animator>().SetTrigger("Run");
+        }
+        
+        if(dist < 2f)
+        {
+            gameObject.GetComponent<Animator>().SetTrigger("Attack");
+        }
+    }
+       void OnTriggerEnter (Collider other)
+     {
+       if (other.tag == "Bullet")
+      {
+          HP = HP - 35;
+     }
+     if (other.tag == "BulletR")
+      {
+          HP = HP - 75;
+     }
+        }
+        void OnTriggerStay (Collider other)
+    {
+        if(other.tag=="Dead")
+        {
+            HP -=Time.deltaTime/10f;
+        }
+    }
+}
