466434f1c9c0fe533ec82ef42c906328d05ab0ca
Eliac11 <58089493+Eliac11@users.noreply.github.com>
2021-01-22 09:14:49 +0300

    поченил двери , пушу пока все работает

--- Assets/Scripts/doorKey.cs
+++ Assets/Scripts/doorKey.cs
@@ -8,22 +8,40 @@ public class doorKey : MonoBehaviour
     public AudioSource audio;
     public AudioClip Open;
 
+    public bool isneedKey = false;
+
     void OnTriggerEnter(Collider other)
     {
-if (other.tag == "Player")
-{
-    if (other.GetComponent<KeyControl>().KeyHas == true)
-    {
-        OpenDoor();
-        
-    }
+        if (other.tag == "Player")
+        {
+            if (isneedKey)
+            {
+                if (other.GetComponent<KeyControl>().KeyHas == true)
+                {
+                    OpenDoor();
+
+                }
+            }
+            else
+            {
+                OpenDoor();
+            }
 }
 
 }
     public void OpenDoor()
     {
         gameObject.GetComponent<Animator>().SetTrigger("Open");
-        audio.PlayOneShot(Open);
+        
     }
     
+
+    //приятно что ты решил заглянуть в этот скрипт ))
+    //вообщем эта фф вызывается из анимации
+    // блин щас русский начнется, а я хз какая у нас тема 
+    public void playaudioOpen()
+    {
+        audio.PlayOneShot(Open);
+    }
+
 }
