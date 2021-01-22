0468a59aba9a9c57e951920a74fc5aed09ad86c8
RebuffedFaun311 <superduperalex18@outlook.com>
2021-01-22 11:30:28 +0300

    Всякие текстуры,модели,скрипт

---
+++ Assets/Scripts/RadioPodbor.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RadioPodbor : MonoBehaviour
+{
+    public Text txt;
+    public string RadioText;
+    public GameObject Radio;
+
+    private bool RadioPod;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            if (RadioPod == false)
+            {
+                Radio.SetActive (true); //нажал -- появилось
+                txt.text = null;
+                txt.text = RadioText;
+                RadioPod = true;
+            } else
+            {
+                Radio.SetActive (false);
+                txt.text = null;  // если еще раз нажал,то пропадает
+                RadioPod = false;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/RadioTrigger.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RadioTrigger : MonoBehaviour
+{
+    public RadioPodbor radioScript;
+    public GameObject Radio;
+
+    void OnTriggerEnter(Collider col)
+    {
+      if (col.tag == "Player")
+      {
+          radioScript.enabled = true; // Заходишь - запрашивает скрипт
+      }  
+    }
+    void OnTriggerExit(Collider col)
+    {
+        if (col.tag == "Player")
+        {
+            radioScript.enabled = false; // выходишь - выключает скрипт и убирает картинку
+            Radio.SetActive (false);
+        }
+    }
+}
