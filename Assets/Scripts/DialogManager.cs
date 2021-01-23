2fce0225a2b80f10e1f8f4bbf21f4188c0073b0a
RebuffedFaun311 <superduperalex18@outlook.com>
2021-01-23 15:00:42 +0300

    Сохранение перед тем,как начать делать курсор

---
+++ Assets/Scripts/DialogManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogManager : MonoBehaviour
+{
+    public GameObject DialogWindow;
+    public DialogStart dialogScript;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            dialogScript.enabled = true;
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            DialogWindow.SetActive (false);
+            dialogScript.enabled = false;
+        }
+    }
+}
