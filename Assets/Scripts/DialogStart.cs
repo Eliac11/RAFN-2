9a34ff0e8514ec9661bcb593bfb0ff251b934fe1
RebuffedFaun311 <superduperalex18@outlook.com>
2021-01-23 15:11:25 +0300

    Revert "Revert "Сохранение перед тем,как начать делать курсор""
    This reverts commit 41343bd25e2ebd747cfe4514b26984eb10bdb18d.

---
+++ Assets/Scripts/DialogStart.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogStart : MonoBehaviour
+{
+    public GameObject DialogWindow;
+
+
+    private bool DialogNachalo;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            if (DialogNachalo == false)
+            {
+                DialogWindow.SetActive (true);
+                DialogNachalo = true;
+            } else
+            {
+                DialogWindow.SetActive (false);
+                DialogNachalo = false;
+            }
+        }
+    }
+}
