b55f4b1710cd80532b11cd6244fbda5ccefcab47
RebuffedFaun311 <superduperalex18@outlook.com>
2021-01-23 16:09:07 +0300

    Диалог

---
+++ Assets/Scripts/DialogSwitch.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogSwitch : MonoBehaviour
+{
+public GameObject window1;
+public GameObject window2;
+public DialogStart dialogScript;
+private bool win1a;
+private bool win2a;
+
+    void Update()
+    {
+        if (dialogScript.enabled = true){
+        if (win2a == true)
+            {
+                window2.SetActive(true);
+            }
+        if (win1a == true)
+            {
+                window1.SetActive(true);
+            }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            win1a=false;
+            window1.SetActive(false);
+            win2a=true;
+        }
+        }
+    }
+}
