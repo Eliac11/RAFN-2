eed62906decbdc18d303a45763c8d5f93732c6c1
Eliac11 <58089493+Eliac11@users.noreply.github.com>
2021-05-15 12:56:13 +0300

    чтото еще

---
+++ Assets/EventsPloshatka.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventsPloshatka : MonoBehaviour
+{
+    public bool isvidno=false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnBecameVisible()
+    {
+        isvidno = !isvidno;
+    }
+}
